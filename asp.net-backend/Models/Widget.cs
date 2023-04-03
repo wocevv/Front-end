66bef9be63397e36c02cac521ca8c1bc5605e288
MatthijsDolmans <95029345+MatthijsDolmans@users.noreply.github.com>
2023-04-03 09:58:52 +0200

    Delete asp.net-backend directory

--- asp.net-backend/Models/Widget.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace asp.net_backend.Models
-{
-    public class Widget
-    {
-        public int Id { get; set; }
-        public string Dataset { get; set; }
-        public string DateRange { get; set; }
-    }
-}
--- asp.net-backend/ZEntityFramework/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace asp.net_backend.EntityFramework
-{
-    public class TodoItem
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
