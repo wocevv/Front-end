932dfcd63ff50ef162e03be443a1f9af4d5f6c2b
matthijs Dolmans <487249@student.fontys.nl>
2023-06-12 13:28:02 +0200

    error fix

--- vue-frontend/src/router/index.ts
+++ vue-frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +10,6 @@ const router = createRouter({
       component: HomeView,
       props: true
       },
-      {
-          path: '/test',
-          name: 'test',
-          component: TestView,
-      }
   ]
 })
 
